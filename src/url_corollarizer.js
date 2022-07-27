6cc47f3f1fd6836002cb4cd3fd996d8161c5c882
JaranF-BL <104915020+JaranF-BL@users.noreply.github.com>
2022-07-27 15:30:13 +0100

    Prune some moribund architecture out of env list

--- src/url_corollarizer.js
+++ src/url_corollarizer.js
@@ -13,12 +13,8 @@ function url_corollarizer(iCorollaryChoice) {
         3) v19b-fe2.dmz.bl.uk \n\n\
         4) v19b-fenle1.dmz.bl.uk \n\
         5) v19b-fenle2.dmz.bl.uk \n\n\
-        6) v19b-dev-fe1.ad.bl.uk \n\
-        7) v19b-dev-fe2.ad.bl.uk \n\n\
         8) v19b-fenle3.dmz.bl.uk \n\
         9) v19b-fenle4.dmz.bl.uk \n\
-        10) v19b-dev-fe2.ad.bl.uk \n\
-        11) v19b-manage.ad.bl.uk \n\n\
         12) LIVE www.bl.uk";
 		while (!iCorollaryChoice || (iCorollaryChoice < 0 || iCorollaryChoice > choices.length)) {
 			iCorollaryChoice = prompt(promptMsg, "");
