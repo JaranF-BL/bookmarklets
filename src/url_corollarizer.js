3a7ed65fbac7b0cc3953beaeefd9e0172c2c6789
Jaran Finn <jaran.finn@bl.uk>
2022-07-28 14:02:34 +0100

    Fixes issue #1 Opened by A.M. Added CDN 2 as a target to jump to and handles clicking cancel

--- src/url_corollarizer.js
+++ src/url_corollarizer.js
@@ -18,10 +18,35 @@ function url_corollarizer(iCorollaryChoice) {
         9) v19b-fenle4.dmz.bl.uk";
 		while (!iCorollaryChoice || (iCorollaryChoice < 0 || iCorollaryChoice > choices.length)) {
 			iCorollaryChoice = prompt(promptMsg, "");
+			if (iCorollaryChoice === null) {
+				blinkWarn();
+				return -1;
+			}
 		}
 	}
 	const currUrl = globalWinObj.location.href;
 	const newUrl = currUrl.replace(/:\/\/([^:/]*)/g, "://" + choices[iCorollaryChoice-1]);
 	globalWinObj.location.href = newUrl;
 	return newUrl;
+}
+
+function blinkWarn() {
+	const ua = navigator.userAgent;
+	const isIEorEdge = ua.indexOf("Edg") > -1 || ua.indexOf("MSIE ") > -1 || /Edge/.test(ua);
+	let bgColor = window.getComputedStyle(document.body).getPropertyValue("background-color");
+	let bgColorRGBonly = bgColor;
+	let arrRGB = [];
+	if (isIEorEdge && (bgColor == 'rgba(0, 0, 0, 0)' || bgColor == 'transparent')) {   
+		bgColorRGBonly = 'rgb(255, 255, 255)';
+	}
+	arrRGB = bgColorRGBonly.match(/\d{1,3}(?:\,)|\d{1,3}/g);
+	if (arrRGB !== null) {
+	  arrRGB = arrRGB.map(function(item) {
+		  let colourVal = Math.abs(parseInt(item) - 255 % 256);
+		  return colourVal; });
+	}
+	const allEqual = arrRGB.every( function(arrayItem) { return arrayItem === arrRGB[0] });
+	if (allEqual && arrRGB[0] > 64 && arrRGB[0] < 192) { arrRGB[0] = arrRGB[0] < 128 ? 0 : 255; } //deal with greys as they don't invert very strikingly
+	document.body.style.backgroundColor = 'rgb(' + arrRGB.join(',') + ')'
+	setTimeout(function() {document.body.style.backgroundColor = bgColor !== bgColorRGBonly ? bgColor : bgColorRGBonly} ,300)
 }
